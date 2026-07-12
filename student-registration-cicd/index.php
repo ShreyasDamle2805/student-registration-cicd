512c819f07b85a4b52fa294bacc7799325cdcc6c
ShreyasDamle2805 <shreyasdamle4@gmail.com>
2026-07-12 19:04:31 +0530

    Changes-db2

--- student-registration-cicd/index.php
+++ student-registration-cicd/index.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Portal</title>
+    <title>Simple Student Portal</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
